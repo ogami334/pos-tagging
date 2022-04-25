8b9f529523636bf019ee71c2de4ec079973a57af
ogami <ogami@ibis1.logos.ic.i.u-tokyo.ac.jp>
2022-04-25 14:41:21 +0900

    added_mat_convert

--- pos_tagging/common/utils.py
+++ pos_tagging/common/utils.py
@@ -1,8 +1,8 @@
 import numpy as np
 def softmax(a):
-    c = np.max(a)
+    c = np.max(a, axis=1, keepdims=True)
     exp_a = np.exp(a-c)
-    sum_exp_a = np.sum(exp_a)
+    sum_exp_a = np.sum(exp_a, axis=1, keepdims=True)
     y = exp_a / sum_exp_a
     return y
 
@@ -19,3 +19,27 @@ def cross_entropy_error(y, t):
         y = y.reshape(1, y.size)
     batch_size = y.shape[0]
     return -np.sum(t * np.log(y + 1e-7)) / batch_size
+
+
+# use case
+# x = np.array([
+#     [1, 2, 3],
+#     [4, 5, 6]])
+# mat_convert(x, 4)
+# result:
+# [[[1 2 3]
+#   [1 2 3]
+#   [1 2 3]
+#   [1 2 3]]
+
+#  [[4 5 6]
+#   [4 5 6]
+#   [4 5 6]
+#   [4 5 6]]]
+def mat_convert(x, s):
+    hoge = []
+    y = np.empty(shape=(s, 3), dtype="int")
+    for i in range(x.shape[0]):
+        v = np.repeat(x[i,:][None, :], s, axis=0)
+        hoge.append(v)
+    return np.stack(hoge)
